981de8c228b4867078e15de7b0335656d94af6b1
Tran Tu <trantu456@gmail.com>
2017-01-22 09:53:44 +0100

    removed consumsers and users urls in main urls

--- apimanager/apimanager/urls.py
+++ apimanager/apimanager/urls.py
@@ -11,8 +11,6 @@ from base.views import HomeView
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name="home"),
     url(r'^oauth/', include('oauth.urls')),
-    url(r'^consumers/', include('consumers.urls')),
-    url(r'^users/', include('users.urls')),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^transactions/', include('transactions.urls')),
     url(r'^payment/', include('payment.urls')),
